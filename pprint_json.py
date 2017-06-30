0fda410bf474cc10c4c93458f5ab59073681cca1
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-06-30 17:32:06 +0500

    done

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
-import json
+import json, sys
 
+filepath = sys.argv[1]
 
 def load_data(filepath):
-    pass
-
+    fo = open(filepath)
+    str = fo.read()
+    data = json.loads(str)
+    return data
 
 def pretty_print_json(data):
-    pass
-
+    data = json.dumps(data, ensure_ascii = False, indent=4)
+    return data
 
 if __name__ == '__main__':
-    pass
+    data = load_data(filepath)
+    print(pretty_print_json(data))
