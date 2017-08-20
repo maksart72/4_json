c1c7a2978a49bdd680a8a4ad9ee1e30b1f664746
maksart72 <maksim.artemyev@gmail.com>
2017-08-20 23:03:50 +0500

    Add JSON exception

--- pprint_json.py
+++ pprint_json.py
@@ -4,10 +4,11 @@ import os
 
 
 def load_json_file(filepath):
-    if not os.path.exists(filepath):
-        return None
-    with open(filepath, 'r') as file_handler:
-        return json.load(file_handler)
+    try:
+        with open(filepath, 'r') as file_handler:
+            return json.load(file_handler)
+    except (OSError, json.JSONDecodeError):
+        return
 
 def pretty_print_json(json_content):
     pretty_json = json.dumps(json_content, ensure_ascii=False, indent=4)
